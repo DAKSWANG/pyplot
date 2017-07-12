81a46c74244e5ac9120c47b89f21eb51a362220f
Daks Wang <dakswang@gmail.com>
2017-07-12 21:15:00 +0800

    sin

---
+++ pyplot2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 21:10:05 2017
+
+@author: DAKS
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# make some data
+x = np.linspace(0, 2*np.pi)
+
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+# plot sin(x) and cos(x)
+fig = plt.figure(figsize=(10,4), dpi=100)
+ax  = fig.add_subplot(111)
+ax.plot(x, y1, c='b', label='y1')
+ax.plot(x, y2, c='r', label='y2')
+
+leg = plt.legend()
+# get the lines and texts inside legend box
+leg_lines = leg.get_lines()
+leg_texts = leg.get_texts()
+# bulk-set the properties of all lines and texts
+plt.setp(leg_lines, linewidth=4)
+plt.setp(leg_texts, fontsize='x-large')
+plt.savefig('leg_example')
+plt.show()
